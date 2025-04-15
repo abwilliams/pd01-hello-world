77b51cb6f118dd748c23d5fe3d4a85594a25ff91
Andrew Williams <williams.ab@gmail.com>
2025-04-15 16:15:14 +0200

    Added api code to index.js.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+// Global variable to store the currently selected currency
+let currentCurrency = "USD";
+
+async function fetchBitcoinPrice() {
+    // Construct the API URL with the current currency
+    const apiUrl = "https://api.coinbase.com/v2/prices/BTC-${currentCurrency}/spot";
+
+    try {
+        // Fetch data from the API
+        let response = await fetch(apiUrl);
+        // Parse the JSON response
+        let data = await response.json();
+        // Extract the bitcoin price from the response
+        const bitcoinPrice = data.data.amount;
+
+        //Updated the price display on the webpage
+        document.getElementById("price").textContent = '${currentCurrency} $${bitcoinPrice}' ;
+
+    } catch (error) {
+        // Log any errors that occur during the fetch operation
+        console.error("Error fetching bitcoin price, error");    
+    }
+}
+
+
 function updateDateTime() {
     // Get the element where the dat/time will be displayed.
     const dateTimeElement = document.getElementById("datetime");
